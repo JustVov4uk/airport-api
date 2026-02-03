b3c3aa9e5821da879c14c2e96f3b4edd6f3d00ba
JustVov4uk <volodabudzan4@gmail.com>
2026-02-03 18:39:38 +0100

    create Airport, Route models. delete __pycache__/ from pushed commit

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255)
+
+
+class Route(models.Model):
+    source = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    distance = models.IntegerField()
