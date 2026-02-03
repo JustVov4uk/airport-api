46334fd75f3a34d5544c37f167116e43d616b1d7
JustVov4uk <volodabudzan4@gmail.com>
2026-02-03 17:48:03 +0100

    create airport, airplane, flight, user, order apps and register them

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "config",
+    "airport",
+    "airplane",
+    "flight",
+    "user",
+    "order",
 ]
 
 MIDDLEWARE = [
