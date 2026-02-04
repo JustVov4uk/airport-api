4eedfb62a2ca30cdaf0eb7e8132bc0456ebd878b
JustVov4uk <volodabudzan4@gmail.com>
2026-02-04 17:12:56 +0100

    add def str and class Meta to airport and route models

--- airport/models.py
+++ airport/models.py
@@ -5,7 +5,21 @@ class Airport(models.Model):
     closest_big_city = models.CharField(max_length=255)
 
 
+    class Meta:
+        verbose_name_plural = "airports"
+
+    def __str__(self):
+        return self.name
+
+
 class Route(models.Model):
     source = models.CharField(max_length=255)
     destination = models.CharField(max_length=255)
     distance = models.IntegerField()
+
+
+    class Meta:
+        verbose_name_plural = "routes"
+
+    def __str__(self):
+        return f"{self.source} -> {self.destination} ({self.distance})"
