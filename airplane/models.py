cabc8d67ac89b4f3c7f1c81cca6e7eceae0b37b9
JustVov4uk <volodabudzan4@gmail.com>
2026-02-04 17:11:37 +0100

    create airplane and airplane type models

--- airplane/models.py
+++ airplane/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "airplanes"
+
+    def __str__(self):
+        return f"{self.airplane_type} -> {self.name} ({self.rows}), ({self.seats_in_row})"
