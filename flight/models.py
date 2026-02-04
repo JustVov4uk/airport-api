fefec8b1c96c00521bd5f4149cffaed86da07224
JustVov4uk <volodabudzan4@gmail.com>
2026-02-04 17:45:41 +0100

    create Crew and Flight models

--- flight/models.py
+++ flight/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
+    class Meta:
+        verbose_name_plural = "crews"
+
+    def __str__(self):
+        return f"{self.first_name} -> {self.last_name}"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey("Route", on_delete=models.CASCADE)
+    airplane = models.ForeignKey("Airplane", on_delete=models.CASCADE)
+    crews = models.ManyToManyField(Crew, related_name="flights")
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    class Meta:
+        verbose_name_plural = "flights"
+
+    def __str__(self):
+        return f"{self.route} -> {self.airplane}"
