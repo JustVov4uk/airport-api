3425b067b13756026540f9f204e09c736b072ef7
JustVov4uk <volodabudzan4@gmail.com>
2026-02-04 18:05:53 +0100

    create AUTH_USER_MODEL in settings.py. Create user model

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,6 +119,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+user = AUTH_USER_MODEL
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
