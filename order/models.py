3afca4ae6823edfe9aa4eecc5022884a9f57307c
JustVov4uk <volodabudzan4@gmail.com>
2026-02-04 18:14:58 +0100

    create Order and Ticket models

--- order/models.py
+++ order/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from flight.models import Flight
+
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "orders"
+
+    def __str__(self):
+        return f"{self.created_at}"
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "tickets"
+
+    def __str__(self):
+        return f"{self.row}-{self.seat}"
