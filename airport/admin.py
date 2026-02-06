d80763d9f639936c1bee670a8b7881e23e938d73
JustVov4uk <volodabudzan4@gmail.com>
2026-02-06 18:03:38 +0100

    register Airport and Route models in admin.py

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import Airport, Route
+
+
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("name", "closest_big_city")
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ("source", "destination", "distance")
