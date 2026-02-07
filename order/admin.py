d91e2bd0e13623c898960501d5520b70e2591e92
JustVov4uk <volodabudzan4@gmail.com>
2026-02-07 19:45:30 +0100

    register Order and Ticket models in admin.py

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from flight.models import Flight
+from order.models import Order, Ticket
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("created_at", "user")
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("row", "seat", "flight", "order")
