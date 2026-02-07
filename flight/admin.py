d7b74af62d94c4ec1a59eb7ae414c46e4b5735a4
JustVov4uk <volodabudzan4@gmail.com>
2026-02-07 19:42:14 +0100

    register Crew and Flight models in admin.py

--- flight/admin.py
+++ flight/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from flight.models import Crew, Flight
+
+
+@admin.register(Crew)
+class CrewAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("route", "airplane", "departure_time", "arrival_time")
