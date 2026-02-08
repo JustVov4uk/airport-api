ab9a79f88bac417f56ea43aab7c9b8300166db3f
JustVov4uk <volodabudzan4@gmail.com>
2026-02-08 15:36:29 +0100

    create basic views for order app(OrderViewSet and TicketViewSet).Create urls.py in order app.

--- order/views.py
+++ order/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from order.models import Order, Ticket
+from order.serializers import OrderSerializer, TicketSerializer
 
-# Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
