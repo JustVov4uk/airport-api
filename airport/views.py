118cf76b8921857b8981671a1929b6a3d7536800
JustVov4uk <volodabudzan4@gmail.com>
2026-02-08 11:41:12 +0100

    create basic views for airport app(AirportViewSet and RouteViewSet).Create urls.py in airport app.

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from airport.models import Airport, Route
+from airport.serializers import AirportSerializer, RouteSerializer
 
-# Create your views here.
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
