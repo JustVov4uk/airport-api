2b76f963047d103c50ea0c286004b1151abfe735
JustVov4uk <volodabudzan4@gmail.com>
2026-02-08 15:22:08 +0100

    create basic views for flight app(CrewViewSet and FlightViewSet).Create urls.py in flight app.

--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from flight.models import Crew, Flight
+from flight.serializers import CrewSerializer, FlightSerializer
 
-# Create your views here.
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
