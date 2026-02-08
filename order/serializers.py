0acd3beb2576d232d3624ff53dcf3741c1489f1f
JustVov4uk <volodabudzan4@gmail.com>
2026-02-08 11:09:38 +0100

    create serializers.py in order app. Create basic serializers for Order and Ticket models(OrderSerializer and TicketSerializer)

---
+++ order/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from order.models import Order, Ticket
+
+
+class OrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Order
+        fields = ("id", "created_at", "user")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "flight", "order")
