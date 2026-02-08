e8b7896f73951ae32383484368b9d3a5144552b4
JustVov4uk <volodabudzan4@gmail.com>
2026-02-08 10:39:28 +0100

    create serializers.py in airplane app. Create basic serializers for AirplaneType and Airplane models(AirplaneTypeSerializer and AirplaneSerializer)

---
+++ airplane/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from airplane.models import AirplaneType, Airplane
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AirplaneType
+        fields = ("id", "name")
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Airplane
+        fields = ("id", "name", "rows", "seats_in_row", "airplane_type", "capacity")
