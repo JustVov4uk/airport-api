864a493a9376eda93fd12d2c6084913c889fa198
JustVov4uk <volodabudzan4@gmail.com>
2026-02-09 19:32:49 +0100

    create permissions.py in airport app. Create custom IsAdminOrReadOnly permission for AirportViewSet and RouteViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from airport.models import Airport, Route
+from airport.permissions import IsAdminOrReadOnly
 from airport.serializers import AirportSerializer, RouteSerializer
 
 
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+    permission_classes = (IsAdminOrReadOnly,)
