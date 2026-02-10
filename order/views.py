926064158251c9bfd8fb04287cbe3c38e7015667
JustVov4uk <volodabudzan4@gmail.com>
2026-02-10 19:27:26 +0100

    create permissions for OrderViewSet and TicketViewSet

--- order/views.py
+++ order/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
 from order.models import Order, Ticket
 from order.serializers import OrderSerializer, TicketSerializer
 
@@ -6,8 +8,22 @@ from order.serializers import OrderSerializer, TicketSerializer
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Order.objects.all()
+        return Order.objects.filter(user_id=user.id)
 
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Ticket.objects.all()
+        return Ticket.objects.filter(order__user=user.id)
