1d58e6b0595483f40567eba558e593137b9d7780
JustVov4uk <volodabudzan4@gmail.com>
2026-02-10 19:10:03 +0100

    add permission classes for CrewViewSet and FlightViewSet. Use custom permission IsAdminOrReadOnly

--- flight/views.py
+++ flight/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+
+from config.permissions import IsAdminOrReadOnly
 from flight.models import Crew, Flight
 from flight.serializers import CrewSerializer, FlightSerializer
 
@@ -6,8 +8,10 @@ from flight.serializers import CrewSerializer, FlightSerializer
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes = (IsAdminOrReadOnly,)
