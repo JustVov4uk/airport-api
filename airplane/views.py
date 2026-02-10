a172f57edcceeb1da1b6ebf715345247363d360e
JustVov4uk <volodabudzan4@gmail.com>
2026-02-10 19:06:08 +0100

    add permission clases for AirplaneTypeViewSet and AirplaneViewSet. Use custom permission IsAdminOrReadOnly

--- airplane/views.py
+++ airplane/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from airplane.models import AirplaneType, Airplane
 from airplane.serializers import AirplaneTypeSerializer, AirplaneSerializer
+from config.permissions import IsAdminOrReadOnly
 
 
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.all()
     serializer_class = AirplaneSerializer
+    permission_classes = (IsAdminOrReadOnly,)
