564086dd68756f719246dde97406cde7236bf73d
JustVov4uk <volodabudzan4@gmail.com>
2026-02-12 18:34:30 +0100

    create tests_route_api.py inside airport/tests folder and write basic tests for Route

--- airport/tests/tests_airport_api.py
+++ airport/tests/tests_airport_api.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from  rest_framework.test import APIClient
-
 from airport.models import Airport
 
 AIRPORT_URL = reverse("airport:airport-list")
