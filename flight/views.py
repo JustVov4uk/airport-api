306c4551818b4e8b8fc63da60cbe7f5f858ec7e5
JustVov4uk <volodabudzan4@gmail.com>
2026-02-20 10:47:13 +0100

    add Flight filtering by source, destination and departure date range

--- flight/views.py
+++ flight/views.py
@@ -1,8 +1,28 @@
+import django_filters
 from rest_framework import viewsets
-
+from django_filters.rest_framework import DjangoFilterBackend
 from config.permissions import IsAdminOrReadOnly
 from flight.models import Crew, Flight
-from flight.serializers import CrewSerializer, FlightSerializer, FlightListSerializer, FlightDetailSerializer
+from flight.serializers import (CrewSerializer,
+                                FlightSerializer,
+                                FlightListSerializer,
+                                FlightDetailSerializer
+                                )
+
+
+class FlightFilter(django_filters.FilterSet):
+    departure_date_from = django_filters.DateFilter(
+        field_name="departure_time",
+        lookup_expr="gte"
+    )
+    departure_date_to = django_filters.DateFilter(
+        field_name="departure_time",
+        lookup_expr="lte"
+    )
+
+    class Meta:
+        model = Flight
+        fields = ["route__source", "route__destination"]
 
 
 class CrewViewSet(viewsets.ModelViewSet):
@@ -15,6 +35,8 @@ class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
     permission_classes = (IsAdminOrReadOnly,)
+    filterset_class = FlightFilter
+
 
     def get_serializer_class(self):
         if self.action == "list":
