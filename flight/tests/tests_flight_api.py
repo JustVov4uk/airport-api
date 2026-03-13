588fec2cddc72056e2b32ab0e528c91cf41b083c
JustVov4uk <volodabudzan4@gmail.com>
2026-03-13 12:05:58 +0100

    feat: add tests for admin Flight API

--- flight/tests/tests_flight_api.py
+++ flight/tests/tests_flight_api.py
@@ -3,7 +3,9 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
-from flight.tests.helpers import create_route, create_airplane, create_flight
+
+from flight.models import Flight
+from flight.tests.helpers import create_route, create_airplane, create_flight, create_crew
 
 FLIGHT_URL = reverse("flight:flight-list")
 
@@ -55,3 +57,50 @@ class AuthorizedFlightApiTests(TestCase):
         }
         result_request = self.client.post(FLIGHT_URL, payload)
         self.assertEqual(result_request.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class AdminFlightApiTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="EMAIL",
+            password="PASSWORD",
+            is_staff=True,
+        )
+        self.client.force_authenticate(user=self.user)
+        self.flight = create_flight()
+
+    def test_create_flight_admin(self):
+        payload = {
+            "route": create_route().id,
+            "airplane": create_airplane().id,
+            "crew": [create_crew().id],
+            "departure_time": "2026-06-01T10:00:00Z",
+            "arrival_time": "2026-06-01T12:00:00Z",
+            "base_price": "100.00"
+        }
+        flight_before = Flight.objects.count()
+        result_request = self.client.post(FLIGHT_URL, payload)
+        self.assertEqual(result_request.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Flight.objects.count(), flight_before + 1)
+
+    def test_update_flight_admin(self):
+        url = flight_detail_url(self.flight)
+        payload = {
+            "route": create_route().id,
+            "airplane": create_airplane().id,
+            "crew": [create_crew().id],
+            "departure_time": "2026-06-01T10:00:00Z",
+            "arrival_time": "2026-06-01T12:00:00Z",
+            "base_price": "150.00"
+        }
+        result_request = self.client.put(url, payload)
+        self.assertEqual(result_request.status_code, status.HTTP_200_OK)
+        self.flight.refresh_from_db()
+        self.assertEqual(self.flight.base_price, 150)
+
+    def test_delete_flight_admin(self):
+        url = flight_detail_url(self.flight)
+        result_request = self.client.delete(url)
+        self.assertEqual(result_request.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Flight.objects.filter(id=self.flight.id).exists())
